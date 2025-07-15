de95f845be2051dfb9fd13b0d5df7aa7609f0acb
Akshat Panchal <akshat.coder.04@gmail.com>
2025-07-15 15:28:42 +0530

    Tested the basic AXIOS API calling & getData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import axios from './utils/axiosconfig'
+import React, { useEffect } from 'react'
 
 const App = () => {
+
+  const getData = async () => {
+    try {
+      const res = await axios.get("/products")
+      const res1 = await axios.get("/users")
+      console.log("Products Response:",res);
+      console.log("Users Response:",res1);
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+  
+
   return (
     <div>App</div>
   )
