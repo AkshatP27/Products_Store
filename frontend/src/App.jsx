af818346ec3b20923f36cc301edffa9af12b6995
Akshat Panchal <akshat.coder.04@gmail.com>
2025-07-21 22:01:20 +0530

    Enhanced user/product fetching logic in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,29 @@ import React, { useEffect } from "react";
 import Mainroutes from "./routes/Mainroutes";
 import Navbar from "./components/Navbar";
 import { asyncCurrentUser } from "./store/actions/userActions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { asyncLoadProducts } from "./store/actions/productActions";
+import { useSearchParams } from "react-router-dom";
 
 const App = () => {
   const dispatch = useDispatch();
 
+  const users = useSelector((state) => state.userReducer.userData);
+  const products = useSelector((state) => state.productReducer.productData);
+
   useEffect(() => {
-    dispatch(asyncCurrentUser());
-    dispatch(asyncLoadProducts());
-  }, [])
-  
+    // If no user data, fetch current user
+    // Advantage:
+    // This line ensures that if there is no user data in the Redux state (for example, after a page reload), the app will automatically try to fetch the current user from localStorage (or another persistent source) and update the Redux state.
+    !users && dispatch(asyncCurrentUser());
+  }, [users]); //Rerenders when users data changes
+
+  useEffect(() => {
+    // If no products data, fetch products
+    // Advantage:
+    // This line ensures that if there are no products loaded in the Redux state (for example, after a page reload or on the first visit), the app will automatically dispatch an action to fetch the products from the backend or API and update the Redux state.
+    products.length == 0 && dispatch(asyncLoadProducts());
+  }, [products]); //Rerenders when products data changes
 
   return (
     <div className="overflow-auto px-[9%] pb-9 w-screen h-screen bg-gray-900 text-white font-thin">
